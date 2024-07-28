f913b9189518529b8e57c951328120380aeb8c84
12SHIVANSH-SINGH <149751092+12SHIVANSH-SINGH@users.noreply.github.com>
2024-07-28 11:48:04 +0000

    javascript 4

---
+++ 01.basic/04_operations.js
@@ -0,0 +1,27 @@
+let value=3
+let negvalue= -value
+// console.log(value);
+// console.log(negvalue);
+// console.log(2+2); //add
+// console.log(2-2); //subtarct
+// console.log(2*2); //prod
+// console.log(2**2); //power
+// console.log(2/3); //quotient
+// console.log(2%3); //rem
+
+let str1="shivansh "
+let str2="singh"
+let str3= str1 + str2
+console.log(str3);
+
+// string first => all treated as string
+// stirng last => first operated then last string conversion
+
+console.log(+true)// allowed 1
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.table({num1,num2,num3});
+
+//unary opeators ++, --
+//postfix and prefix
